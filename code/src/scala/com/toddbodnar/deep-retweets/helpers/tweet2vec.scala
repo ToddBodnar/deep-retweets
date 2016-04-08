e98ed158691d70a129129703f515d56c09586631
ToddBodnar <tjb5215@psu.edu>
2016-04-08 13:02:51 -0700

    optimization
    getVectors is super slow for some reason!

--- code/src/scala/com/toddbodnar/deep-retweets/helpers/tweet2vec.scala
+++ code/src/scala/com/toddbodnar/deep-retweets/helpers/tweet2vec.scala
@@ -3,11 +3,11 @@
  **/
 import scala.util.Try
 
-def tweet2vec( word2vec:org.apache.spark.mllib.feature.Word2VecModel, text:Seq[String]) : List[Float] = {
+def tweet2vec( word2vec:org.apache.spark.mllib.feature.Word2VecModel, text:Seq[String]) : List[Double] = {
 	//try/catch/filter is to deal with rare words that are not in the model
-	val vectors = text.map(word => try{word2vec.getVectors(word)}catch{case e : Exception => None}).filter(_ != None).map(_.asInstanceOf[Array[Float]])
+	val vectors = text.map(word => try{word2vec.transform(word)}catch{case e : Exception => None}).filter(_ != None).map(_.asInstanceOf[org.apache.spark.mllib.linalg.DenseVector].toArray)
 	if (vectors.size == 0)
-		return List.fill[Float](100)(0)
+		return List.fill[Double](100)(0)
 	val result = List.tabulate(vectors(0).size)(n => vectors.map(vector => vector(n)).sum / vectors.size)
 	return result
 }
